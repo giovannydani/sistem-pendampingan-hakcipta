dbafceca1c06461cf1725c9dfd9fb5e3d962b16c
Giovanny Dani Saputra <giovannysaputra14@gmail.com>
2023-05-27 18:52:44 +0700

    fixing admin detail ajuan

--- resources/views/admin/ajuan/detail.blade.php
+++ resources/views/admin/ajuan/detail.blade.php
@@ -212,7 +212,7 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    @if ($detailHakcipta->newcomment->comment)
+                    @if ($detailHakcipta->newcomment)
                     <div class="col-md-12">
                         <label class="mt-2" >Last Comment</label >
                     </div>
